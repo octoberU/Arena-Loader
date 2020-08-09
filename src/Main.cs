fe2ba05bd011d9e32ff156ccf5dd59fb422622ae
octo <allblvnk@gmail.com>
2020-08-09 16:18:52 +0100

    Version bump

--- src/Main.cs
+++ src/Main.cs
@@ -14,7 +14,7 @@ namespace AudicaModding
             public const string Name = "ArenaLoader";  // Name of the Mod.  (MUST BE SET)
             public const string Author = "octo"; // Author of the Mod.  (Set as null if none)
             public const string Company = null; // Company that made the Mod.  (Set as null if none)
-            public const string Version = "0.1.3"; // Version of the Mod.  (MUST BE SET)
+            public const string Version = "0.1.4"; // Version of the Mod.  (MUST BE SET)
             public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none
         }
 
